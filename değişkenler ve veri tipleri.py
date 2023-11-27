d884403df99b1f2c3a9a245d0351fdb8ab094c2a
KemalTanDemirel <147979119+KemalTanDemirel@users.noreply.github.com>
2023-11-27 23:03:44 +0300

    Update

---
+++ değişkenler ve veri tipleri.py
@@ -0,0 +1,28 @@
+Değişkenler ve Veri Tipleri:Değişkenler (Variables):Python'da bir değişken oluşturmak için isim belirleyip değer atarsın. Değişken isimleri harf veya alt çizgi ile başlamalıdır ve boşluk içermemelidir. İşte bir örnek:# Değişken oluşturma
+isim = "Ahmet"
+yas = 25
+pi_sayisi = 3.14
+dogru_mu = TrueVeri Tipleri (Data Types):Python'da yaygın olarak kullanılan bazı veri tipleri şunlardır:Integer (Tam Sayı):sayi = 42Float (Ondalıklı Sayı):pi = 3.14String (Metin):isim = "Ahmet"Boolean (Mantıksal Değer):dogru_mu = TrueList (Liste):meyveler = ["elma", "armut", "çilek"]Tuple (Demet):koordinat = (3, 5)Set (Küme):renkler = {"kırmızı", "yeşil", "mavi"}Dictionary (Sözlük):ogrenci = {"ad": "Ayşe", "yas": 22, "not": 85.5}Değişken Oluşturma Örnekleri:# String (Metin)
+isim = "Ahmet"
+soyisim = "Yılmaz"
+
+# Integer (Tam Sayı)
+yas = 25
+
+# Float (Ondalıklı Sayı)
+ortalama = 85.5
+
+# Boolean (Mantıksal Değer)
+dogru_mu = True
+
+# Liste (List)
+notlar = [90, 85, 75, 95]
+
+# Tuple (Demet)
+koordinat = (3, 5)
+
+# Set (Küme)
+renkler = {"kırmızı", "yeşil", "mavi"}
+
+# Dictionary (Sözlük)
+ogrenci = {"ad": "Ayşe", "yas": 22, "not": 85.5}Bu örneklerde farklı veri tiplerinde değişkenler görüyorsun. Python, değişkenlere değer atarken veri tipini otomatik olarak belirler. Bu özelliği sayesinde Python'da değişken tanımlarken veri tipini belirtmek zorunlu değildir.
